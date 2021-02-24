66d851f31ff40fafdc083e553a2841c128573993
prodigysurge <boatinglife6@yahoo.com>
2021-02-24 14:22:03 -0500

    commit tree 3e18799d16cae932561cd034b89ee87d4c5d4c94

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+    configure do
+        enable :sessions
+        set :session_secret, 'jimmy_crack_corn_and_i_dont_care'
+    end
+
+    get '/' do 
+        erb :index
+    end
+
+    post '/checkout' do 
+        session[:item] = params[:item]
+        @session = session
+        erb :checkout
+    end
+
+
 end
